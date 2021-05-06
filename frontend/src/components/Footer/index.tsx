af8fa67a95f00827212d3fa7ed1e9d857557bd92
crondinoni <carlo.rondinoni@gmail.com>
2021-05-06 00:39:11 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/crondinoni" target="_blank" rel="noreferrer">Carlo Rondinoni</a></p>
+                <p className="text-light"><small><strong>Tagmavisions na Semana Spring React</strong><br />
+      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
