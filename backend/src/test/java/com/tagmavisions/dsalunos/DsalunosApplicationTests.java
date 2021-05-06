2e57aed0e988a89ce758baa420f43ee1508f51cc
crondinoni <carlo.rondinoni@gmail.com>
2021-05-05 22:51:13 -0300

    Project created

---
+++ backend/src/test/java/com/tagmavisions/dsalunos/DsalunosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tagmavisions.dsalunos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsalunosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
