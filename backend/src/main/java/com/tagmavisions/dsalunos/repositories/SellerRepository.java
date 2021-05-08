d65c21a2c5994a49ead00f56cdcf121dda3c797c
crondinoni <carlo.rondinoni@gmail.com>
2021-05-08 14:45:12 -0300

    Layers

---
+++ backend/src/main/java/com/tagmavisions/dsalunos/repositories/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.tagmavisions.dsalunos.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tagmavisions.dsalunos.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long> {
+
+}
