5872c88978a691cab6cf35c00fea4dc2756192e9
crondinoni <carlo.rondinoni@gmail.com>
2021-05-09 10:27:41 -0300

    DataTable integration

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -1,4 +1,26 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { SalePage } from "types/sale";
+import { formatLocalDate } from "utils/format";
+import { BASE_URL } from "utils/requests";
+
 const DataTable = () => {
+
+    const [page, setPage] = useState<SalePage>({
+        first: true,
+        number: 0,
+        last: true,
+        totalElements: 0,
+        totalPages: 0,
+    });
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales?page=0&size=20&sort=date,desc`)
+            .then(response => {
+                setPage(response.data);
+            });
+    }, []);
+
     return (
         <div className="table-responsive">
             <table className="table table-striped table-sm">
@@ -12,34 +34,15 @@ const DataTable = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>06/05/2021</td>
-                        <td>A. T. Yeung</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017</td>
-                    </tr>
-                    <tr>
-                        <td>15/05/2021</td>
-                        <td>S. Ruetten</td>
-                        <td>32</td>
-                        <td>23</td>
-                        <td>14899</td>
-                    </tr>
-                    <tr>
-                        <td>27/05/2021</td>
-                        <td>P. C. McAfee</td>
-                        <td>29</td>
-                        <td>18</td>
-                        <td>13541</td>
-                    </tr>
-                    <tr>
-                        <td>31/05/2021</td>
-                        <td>H. M. Mayer</td>
-                        <td>23</td>
-                        <td>12</td>
-                        <td>11244</td>
-                    </tr>
+                    {page.content?.map(item => (
+                        <tr key={item.id}>
+                            <td>{formatLocalDate(item.date, "dd/MM/yyyy")}</td>
+                            <td>{item.seller.name}</td>
+                            <td>{item.visited}</td>
+                            <td>{item.deals}</td>
+                            <td>{item.amount.toFixed(2)}</td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
         </div>
--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,6 +1,29 @@
+import { Seller } from "./seller"
+
+export type Sale = {
+    id: number;
+    visited: number;
+    deals: number,
+    amount: 5501.0;
+    date: string;
+    seller: Seller;
+}
+
+export type SalePage = {
+    content?: Sale[];
+    last: boolean;
+    totalElements: number;
+    totalPages: number;
+    size?: number;
+    number: number;
+    numberOfElements?: number;
+    first: boolean;
+    empty?: boolean;
+}
+
 export type SaleSum = {
     sellerName: string;
-    sum: number
+    sum: number;
 }
 
 export type SaleSuccess = {
